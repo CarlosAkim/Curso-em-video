0be16668017c4c086464fbc92ec4eaa266caf777
Carlos Henrique dias Akim <ch.akim1994@gmail.com>
2023-07-24 14:09:16 -0300

    Atualizando

--- trabalho de conclusão/conclusão.js
+++ trabalho de conclusão/conclusão.js
@@ -1,35 +1,36 @@
-let valor = document.querySelector("input#valor")
-    let tab = document.querySelector("select#tabela")
-    let v1 = []
-    
+var numero = document.querySelector("#fnum")
+var tabela = document.querySelector("#tabela")
+var res = document.querySelector("#finalizar")
+var valor = []
 
-    function inNumero(n){
-        if(Number(n.value) >= 1 &&  Number(n.value)<= 100) {
-            return true
-        }else {
-            return false
-        }
-    }
-
-    function inLista(n,c) {
-        if(c.indexOf(Number(n.value)) != -1){
-            return true
-        }else {
-            return false
-        }
-    }
-function adicionar(){
-    if (inNumero(valor.value) && !inLista(valor.value, v1)) {
-        alert('ok')
+function inValor(n){
+    //se o valor "n" foi maior que 1 e menor ou igual a 100, retorna "true"
+    if (Number(n) >=1 && Number(n) <= 100){
+        return true
     }else{
-        alert('temos um problema.')
+        return false
+    }
+}
+function inLista(x, c){
+    // Se o valor de "x" estiver dentro de "c" for diferente de "-1" retorna "true"
+    if(c.indexOf(Number(x)) != -1){
+        return true
+    }else{
+        return false
     }
 }
 
-/*
-function limpar(){
-    let d = document.querySelector("#dados")
-    d.innerHTML = `Ao todo foram ${tab.length} cadastrados.`
+function Adicionar(){
+  
+    if (inValor(numero.value) && !inLista(numero.value, valor)){
+        valor.push(Number(numero.value))
+        let tab = document.createElement("option")
+        tab.innerHTML = `Foi adicionado o valor de ${Number(numero.value)}`
+        tabela.appendChild(tab)
+    }else{
+        alert('temos um erro')
+    }
+        
+
 
 }
-*/
